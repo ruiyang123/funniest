c33d8f71f0c0425f91238bd0f1575297ceb4151a
ruiyang123 <rui.yang.ecl@gmail.com>
2020-08-26 15:10:11 +0200

    package

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/ruiyang123/funniest',
+      author='ruiYang',
+      author_email='rui.yang.ecl@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
